f601437cb2f2c8dc19de8f446edafaaa4770351f
Sarah Creager <sarah.f.jamieson@gmail.com>
2021-06-23 20:04:15 -0700

    used functions to improve clarity and readability of code

--- app.js
+++ app.js
@@ -1,6 +1,32 @@
 // asking user for their name
 
-let username = prompt('Please enter your name:');
-console.log(username);
-document.write('<h2>' + username + '</h2>');
+function getusername() {
+    let username = prompt('Please enter your name:');
+    console.log(username);
+    document.write('<h2>' + username + '</h2>');
+    return username; // -> 'sarah'
+}
 
+
+function getuserAnswer(){
+    let userAnswer = prompt('Do you listen to any music from independent artists?');
+    console.log(userAnswer);
+    
+    if (userAnswer.toLowerCase() == 'yes'){
+        document.write('<h3>Thank you for supporting indie music! People like you allow independent artists to continue creating!' + '</h3>');
+        document.write('<img src="https://i.ibb.co/wJhxPTb/Thank-you-2.png" alt="Thank-you" />')
+    } else {
+        document.write('<h3>I challenge you to listen to one independent artist today. You may just find your new favorite song!' + '</h3>')
+    }  
+  }
+
+  function getuserAnswer2(){
+    var txt;
+    var r = confirm("Continue to site if you love music!");
+      if (r == true) {
+      txt = "You pressed OK!!";
+  } else {
+      txt = "You pressed Cancel!";
+  }
+  }
+  
